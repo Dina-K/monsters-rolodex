26f3051ac7e17f9df1ad37cfd202ca59812aa346
Dina_K <edina.kovats0622@live.com>
2020-07-18 23:13:21 +0100

    Updates

---
+++ precache-manifest.5353f453cf106e57fcae157d54c010c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db89650211c7fa9fdf24193a0a2c2303",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ea90f359eb439639660f",
+    "url": "/monsters-rolodex/static/css/main.2451633c.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea90f359eb439639660f",
+    "url": "/monsters-rolodex/static/js/main.76a7c72d.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
